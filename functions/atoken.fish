1bb2810686e834308284dbf380dc077fcd7d033b
umatare5 <umatare5@gmail.com>
2022-01-17 22:04:34 +0900

    Update atoken

--- functions/atoken.fish
+++ functions/atoken.fish
@@ -1,4 +1,4 @@
 function atoken --wraps=jq\ -r\ \'.region\ +\ \'\|\'\ +\ .expiresAt\'\ /\(ls\ -rt\ \|\ tail\ -n\ 1\) --wraps=jq\ -r\ \'.region\ +\ \"\|\"\ +\ .expiresAt\'\ /\(ls\ -rt\ \ \|\ tail\ -n\ 1\) --description alias\ atoken=jq\ -r\ \'.region\ +\ \"\|\"\ +\ .expiresAt\'\ /\(ls\ -rt\ \ \|\ tail\ -n\ 1\)
   set AWS_CACHE_DIR ~/.aws/sso/cache
-  jq -r '.region + "|" + .expiresAt' /(ls -rt | tail -n 1) $argv;
+  jq -r '.region + "|" + .expiresAt' /(ls -rt $AWS_CACHE_DIR | tail -n 1) $argv;
 end
