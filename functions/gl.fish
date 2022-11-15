fe02654779c8a70a669112994e9fd73eba30b482
umatare5 <umatare5@gmail.com>
2022-11-16 01:18:28 +0900

    Improve gl command

--- functions/gl.fish
+++ functions/gl.fish
@@ -1,3 +1,3 @@
-function gl --wraps='git log --oneline --graph --decorate' --description 'alias gl=git log --oneline --graph --decorate'
-  git log --oneline --graph --decorate $argv; 
+function gl --wraps='git log --graph --format --date --decorate-refs' --description 'alias gl=git log --graph --format --date --decorate-refs'
+  git log --graph --pretty='format:%C(yellow)%h %C(white)%s %C(red)%d%C(green)<%an> %C(blue)[%ad]' --date=relative --decorate-refs=tags $argv;
 end
