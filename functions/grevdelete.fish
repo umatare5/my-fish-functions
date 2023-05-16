31b9589a4abf28dc662496c6abe2365e5784245f
umatare5 <umatare5@gmail.com>
2023-05-17 03:33:10 +0900

    Fix

--- functions/grevdelete.fish
+++ functions/grevdelete.fish
@@ -1,8 +1,8 @@
-function grevdelete -a serviceName -a revisionName -a regionName --description 'alias grevdelete=gcloud run revisions delete serviceName revisionName --region regionName'
+function grevdelete -a revisionName -a regionName --description 'alias grevdelete=gcloud run revisions delete revisionName --region regionName'
 
   # Validation
   if ! _has_over_two_arguments $argv
-    set_color red; echo "Syntax failed: grevdelete serviceName revisionName regionName"
+    set_color red; echo "Syntax failed: grevdelete revisionName regionName"
     return 1
   end
 
@@ -11,6 +11,6 @@ function grevdelete -a serviceName -a revisionName -a regionName --description '
   set SERVICE_NAME  $serviceName
   set REGION_NAME   (_gcloud_select_region_name $regionName)
 
-  echo "Running 'gcloud run revisions delete $SERVICE_NAME --region $REGION_NAME $REVISION_NAME' ..."
-  gcloud run revisions delete $SERVICE_NAME --region $REGION_NAME $REVISION_NAME;
+  echo "Running 'gcloud run revisions delete $REVISION_NAME --region $REGION_NAME' ..."
+  gcloud run revisions delete $REVISION_NAME --region $REGION_NAME;
 end
