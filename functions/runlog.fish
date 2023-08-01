e278f7ee8a78d31696ecea5a17c0e77933ff8847
umatare5 <umatare5@gmail.com>
2023-08-01 18:24:36 +0900

    Update runlog

--- functions/runlog.fish
+++ functions/runlog.fish
@@ -1,8 +1,8 @@
-function runlog -a serviceName --description 'alias runlog=gcloud logging read "resource.type=cloud_run_revision" --format json'
+function runlog -a serviceName -a limit --description 'alias runlog=gcloud logging read "resource.type=cloud_run_revision" --format json --limit'
 
   # Validation
   if ! _has_over_one_arguments $argv
-    set_color red; echo "Syntax failed: runshow serviceName regionName"
+    set_color red; echo "Syntax failed: runshow serviceName"
     return 1
   end
 
@@ -11,9 +11,15 @@ function runlog -a serviceName --description 'alias runlog=gcloud logging read "
 
   gcloud logging read \
     "resource.type=cloud_run_revision AND resource.labels.service_name=$SERVICE_NAME" \
-    --format json $argv;
+    --limit (_gcloud_select_limit $limit) \
+    --format json | \
+    jq '[.[] | {insertId, severity, timestamp, logName, textPayload, statusMessage: .protoPayload.status.message }]';
 end
 
 function _has_over_one_arguments
   test (count $argv) -gt 0; and true; or false;
+end
+
+function _gcloud_select_limit -a limit
+  test $limit; and echo $limit; or echo 10;
 end
