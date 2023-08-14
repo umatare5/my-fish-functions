91693ab29d1a825f342a038866b756884f2092f2
umatare5 <umatare5@gmail.com>
2023-08-14 15:11:45 +0900

    Add

--- functions/textize-runapplog.fish
+++
@@ -1,3 +0,0 @@
-function textize-runapplog --description 'alias textize-runapplog=gcloud logging read "resource.type=cloud_run_revision" --format json --limit'
-  jq -r '.[] | "\\(.timestamp)\t\\(.severity)\t\\(.textPayload)"' | sort
-end
--- functions/textize-runsyslog.fish
+++
@@ -1,3 +0,0 @@
-function textize-runsyslog --description 'alias textize-runsyslog=gcloud logging read "resource.type=cloud_run_revision" --format json --limit'
-  jq -r '.[] | "\\(.timestamp)\t\\(.severity)\t\\(.protoPayload.status.message)"' | sort
-end
