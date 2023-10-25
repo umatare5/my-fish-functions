a1487f43183c44f0e5b124f08fb10c6c83accdf7
umatare5 <umatare5@gmail.com>
2023-10-25 20:26:58 +0900

    Add hg

--- functions/hg.fish
+++ functions/hg.fish
@@ -8,16 +8,20 @@ function hg -a resourceName -a resourceKey --description 'alias hg=rg --color ne
 
   # Run command
   set RESOURCE_NAME resourceName
-  set RESOURCE_KEY  resourceKey
+  set RESOURCE_KEY  (_gcloud_select_region_name $resourceKey)
 
   for filename in (rg --color never --files-with-matches "resource \"$RESOURCE_NAME\"")
     for resource_id in (cat $filename | hcledit block list)
+
+    test $RESOURCE_KEY;
       cat $filename | hcledit attribute get $resource_id$RESOURCE_KEY
+      cat $filename | hcledit block get $resource_id
+
     end
   end
 end
 
-function _gcloud_select_region_name -a regionName
+function _hclgrep_select_resource_key -a regionName
   test $resourceKey; and echo ".$resourceKey"; or echo "";
 end
 
