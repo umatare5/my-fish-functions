169975b8d5a1b43b5c9c90f43771f3997cd9d980
umatare5 <umatare5@gmail.com>
2023-12-11 13:51:57 +0900

    Improve runconlog

--- functions/runconlog.fish
+++ functions/runconlog.fish
@@ -8,12 +8,14 @@ function runconlog -a serviceName -a limit --description 'alias runconlog=gcloud
     end
 
     # Run command
+    set GCP_PROJECT_ID (gcloud config get-value project)
     set SERVICE_NAME $serviceName
 
-    gcloud logging read \
-        "resource.type: cloud_run_revision AND resource.labels.service_name: $SERVICE_NAME AND jsonPayload.message: *" \
-        --limit (_gcloud_select_limit $limit) \
-        --format json
+    gcloud logging read " \
+      resource.type=cloud_run_revision AND \
+      resource.labels.service_name=$SERVICE_NAME AND \
+      logName=projects/$GCP_PROJECT_ID/logs/run.googleapis.com%2F%2Fdev%2Flog \
+    " --limit (_gcloud_select_limit $limit) --format json | jq -r '.[] | "\(.timestamp)\\t\(.jsonPayload)"'
 end
 
 function _has_over_one_arguments
