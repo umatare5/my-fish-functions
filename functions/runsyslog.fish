7824cefa17562345130d78abddffbcd099341907
umatare5 <umatare5@gmail.com>
2023-12-11 14:34:56 +0900

    Rename `runconlog` to `runsyslog`

--- functions/runsyslog.fish
+++ functions/runsyslog.fish
@@ -8,12 +8,14 @@ function runsyslog -a serviceName -a limit --description 'alias runsyslog=gcloud
     end
 
     # Run command
+    set GCP_PROJECT_ID (gcloud config get-value project)
     set SERVICE_NAME $serviceName
 
-    gcloud logging read \
-        "resource.type: cloud_run_revision AND resource.labels.service_name: $SERVICE_NAME AND protoPayload.status.message: *" \
-        --limit (_gcloud_select_limit $limit) \
-        --format json
+    gcloud logging read " \
+      resource.type=cloud_run_revision AND \
+      resource.labels.service_name=$SERVICE_NAME AND \
+      logName=projects/$GCP_PROJECT_ID/logs/run.googleapis.com%2Fvarlog%2Fsystem \
+    " --limit (_gcloud_select_limit $limit) --format json | jq -r '.[] | "\(.timestamp)\\t\(.severity)\\t\(.textPayload)"'
 end
 
 function _has_over_one_arguments
