f0202e5a2b06ab268f90163abe6901b827d8d087
umatare5 <umatare5@gmail.com>
2023-12-11 14:12:02 +0900

    Update

--- functions/runapplog.fish
+++ functions/runapplog.fish
@@ -10,10 +10,14 @@ function runapplog -a serviceName -a limit --description 'alias runapplog=gcloud
     # Run command
     set SERVICE_NAME $serviceName
 
-    gcloud logging read \
-        "resource.type: cloud_run_revision AND resource.labels.service_name: $SERVICE_NAME AND textPayload: *" \
-        --limit (_gcloud_select_limit $limit) \
-        --format json
+    gcloud logging read "\
+      resource.type=cloud_run_revision AND \
+      resource.labels.service_name=$SERVICE_NAME AND \
+      logName=( \
+        projects/$GCP_PROJECT_ID/logs/run.googleapis.com%2Fstderr OR \
+        projects/$GCP_PROJECT_ID/logs/run.googleapis.com%2Fstdout
+      )" --limit (_gcloud_select_limit $limit) --format json \
+        | jq -r '.[] | if .textPayload then "\(.timestamp)\\t\(.textPayload)" else "\(.timestamp)\\t\(.jsonPayload)" end'
 end
 
 function _has_over_one_arguments
