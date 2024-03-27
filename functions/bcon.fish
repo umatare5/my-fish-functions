29e554a045ae93a7cea98deec6dee89860b6d8fd
umatare5 <umatare5@gmail.com>
2024-03-27 20:31:01 +0900

    Airport has been dead in MacOS 14.4

--- functions/bcon.fish
+++ functions/bcon.fish
@@ -1,3 +1,12 @@
 function bcon --wraps='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s' --description 'alias bcon=/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s'
-    /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s $argv
+    /usr/sbin/system_profiler SPAirPortDataType -json | jq '.SPAirPortDataType[0].spairport_airport_interfaces[0].spairport_airport_other_local_wireless_networks' | jq '[.[] |
+        {
+          "name": ._name,
+          "security": (.spairport_security_mode | sub("s?pairport_security_mode_"; "")),
+          "channel": (.spairport_network_channel | split(" ")[0] + " ch"),
+          "channelBand": (.spairport_network_channel | split(" ")[1] | sub("[(]"; "") | sub("GHz"; " GHz") | sub("[,]"; "")),
+          "channelWidth": (.spairport_network_channel | split(" ")[2] | sub("[)]"; "") | sub("MHz"; " MHz")),
+          "signal": (.spairport_signal_noise | split(" / ")[0] | sub (" dBm"; "") + " dBm"),
+          "noise": (.spairport_signal_noise | split(" dBm / ")[1] | sub (" dBm"; "") + " dBm"),
+        }] | sort_by(.name)'
 end
--- functions/wlan.fish
+++ functions/wlan.fish
@@ -1,3 +1,16 @@
 function wlan --wraps='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I' --description 'alias wlan=/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I'
-    /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I $argv
+    /usr/sbin/system_profiler SPAirPortDataType -json | jq '.SPAirPortDataType[0].spairport_airport_interfaces[0].spairport_current_network_information' | jq '[. |
+        {
+          "name": ._name,
+          "security": (.spairport_security_mode | sub("s?pairport_security_mode_"; "")),
+          "countryCode": .spairport_network_country_code,
+          "channel": (.spairport_network_channel | split(" ")[0] + " ch"),
+          "channelBand": (.spairport_network_channel | split(" ")[1] | sub("[(]"; "") | sub("GHz"; " GHz") | sub("[,]"; "")),
+          "channelWidth": (.spairport_network_channel | split(" ")[2] | sub("[)]"; "") | sub("MHz"; " MHz")),
+          "signal": (.spairport_signal_noise | split(" / ")[0] | sub (" dBm"; "") + " dBm"),
+          "noise": (.spairport_signal_noise | split(" dBm / ")[1] | sub (" dBm"; "") + " dBm"),
+          "phyMode": .spairport_network_phymode,
+          "dataRate": (.spairport_network_rate | tostring + " Mbps"),
+        }]'
+
 end
