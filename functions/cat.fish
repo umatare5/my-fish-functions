1cf1cec54a9e022b39085ab9d60a3bd3c60ff190
umatare5 <umatare5@gmail.com>
2024-04-09 18:11:44 +0900

    Apply theme to `bat`

--- functions/cat.fish
+++ functions/cat.fish
@@ -1,3 +1,3 @@
 function cat --wraps='bat --style plain --paging never' --description 'alias cat=bat --style plain --paging never'
-    bat --style plain --paging never $argv
+    bat --style plain --paging never --theme "Monokai Extended Bright" $argv
 end
